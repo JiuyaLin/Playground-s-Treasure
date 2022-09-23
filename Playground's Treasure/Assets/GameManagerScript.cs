4f03cce57d14281277e258fe3183cbefbabb59a8
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2022-09-23 13:50:53 -0400

    Add Movement

--- Playground's Treasure/Assets/GameManagerScript.cs
+++ Playground's Treasure/Assets/GameManagerScript.cs
@@ -4,30 +4,15 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-    public float speed;
-    public float hspeed = 0;
-    public float vspeed = 0;
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-
     // Start is called before the first frame update
-   private void Start()
+    void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        
     }
-
 
     // Update is called once per frame
-   void Update()
-{
-    Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-    controller.Move(move * Time.deltaTime * speed);
-
-    if (move != Vector3.zero)
+    void Update()
     {
-        gameObject.transform.forward = move;
+        
     }
 }
-
-}
-
---
+++ Playground's Treasure/Assets/MoveCollideScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCollideScript: MonoBehaviour
+{
+    public float speed;
+    public float hspeed = 0;
+    public float vspeed = 0;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+
+    // Start is called before the first frame update
+   private void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+
+    // Update is called once per frame
+   void Update()
+{
+    Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    controller.Move(move * Time.deltaTime * speed);
+
+    if (move != Vector3.zero)
+    {
+        gameObject.transform.forward = move;
+    }
+}
+
+}
+
