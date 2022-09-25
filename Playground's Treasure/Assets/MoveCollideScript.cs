aa508c9a5204fc6c82b57025b5313f2ebd5de707
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2022-09-25 13:49:14 -0400

    Update MoveCollideScript.cs

--- Playground's Treasure/Assets/MoveCollideScript.cs
+++ Playground's Treasure/Assets/MoveCollideScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class MoveCollideScript: MonoBehaviour
@@ -7,27 +8,31 @@ public class MoveCollideScript: MonoBehaviour
     public float speed;
     public float hspeed = 0;
     public float vspeed = 0;
-    private CharacterController controller;
+  
     private Vector3 playerVelocity;
 
     // Start is called before the first frame update
-   private void Start()
+    private void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        controller = gameObject.AddComponent<controller>();
+
     }
-
-
     // Update is called once per frame
-   void Update()
-{
-    Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-    controller.Move(move * Time.deltaTime * speed);
-
-    if (move != Vector3.zero)
+    void Update()
     {
-        gameObject.transform.forward = move;
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * speed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
     }
-}
+
+    
+
 
 }
+
+
 
