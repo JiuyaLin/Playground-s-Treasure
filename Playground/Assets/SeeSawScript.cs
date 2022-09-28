e8b30d0f5fe260880c2e43352fd2fd9433071f7d
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2022-09-28 16:51:02 -0400

    Almost Done

--- Playground/Assets/SeeSawScript.cs
+++ Playground/Assets/SeeSawScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class SeeSawScript : MonoBehaviour
 {
+    public GameManagerScript gameManager;
+    public AudioSource audioSource;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "worm")
+        {
+            Destroy(this.gameObject);
+            print("hit");
+
+
+            gameManager.wrong.Play();
+
+        }
     }
 }
