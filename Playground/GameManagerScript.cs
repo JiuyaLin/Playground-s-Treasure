867a3cd1cada20011c75b051658ea37a3ff160a1
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2022-09-27 23:06:53 -0400

    Hurry Up

--- Playground/GameManagerScript.cs
+++ Playground/GameManagerScript.cs
@@ -1,29 +1,27 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
     public GameObject worm;
-    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void OnCollisionEnter2D(Collision2D collision)
+    public void OnCollisionEnter2D(Collision collision)
     {
 
         Destroy(collision.gameObject);
-        print("hit");
-        
+        Debug.log("hit");
+
     }
 }
