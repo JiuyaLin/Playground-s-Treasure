d94da4981d25df0d43826fdbc51cf870132fe80a
Cosmocs <101135810+Cosmocs@users.noreply.github.com>
2022-10-05 11:18:02 -0400

    Fix Destroying Tiles

--- Playground/Assets/SlideScrit.cs
+++ Playground/Assets/SlideScrit.cs
@@ -16,17 +16,16 @@ public class SlideScrit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
- 
+
+
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "worm")
         {
-            Destroy(this.gameObject);
             print("hit");
-           
+            Destroy(GameObject.FindWithTag("Finish"));
 
 
 
